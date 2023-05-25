577ea7f6b4e72a3548d057ec928c240536deecb3
Tania Aktar Jhoma <tania.aktar.cse@ulab.edu.bd>
2023-05-25 14:40:35 +0600

    Js

---
+++ main.js
@@ -0,0 +1,44 @@
+
+        $(document).ready(function() {
+    // Smooth scroll to section
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $($(this).attr('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 800);
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+var scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+// Show or hide the button based on the scroll position
+window.addEventListener('scroll', function() {
+if (window.pageYOffset > 300) {
+  scrollToTopBtn.style.display = 'block';
+} else {
+  scrollToTopBtn.style.display = 'none';
+}
+});
+
+// Scroll to the top when the button is clicked
+scrollToTopBtn.addEventListener('click', function() {
+window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+});
+$(document).ready(function(){
+    $('.slider').slick({
+      prevArrow: '<div class="slick-prev"></div>',
+      nextArrow: '<div class="slick-next"></div>'
+    });
+  });
+    
+
+
+
+      
+        
+      
